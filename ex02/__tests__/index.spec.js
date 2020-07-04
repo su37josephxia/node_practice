c39c6c32f9a59ee2522a94711fbc962a83acffa6
xiaran <xiaran@polomi.com>
2020-07-04 09:57:27 +0800

    update add 三个函数测试用例

--- ex02/__tests__/index.spec.js
+++ ex02/__tests__/index.spec.js
@@ -73,4 +73,35 @@ describe('练习02 中间件实现', () => {
         expect(calls[2][0]).toBe('2 end');
         expect(calls[3][0]).toBe('1 end');
     })
+
+    it('三个函数的情况' ,async () => {
+        const mockFn = jest.fn()
+        const middlewares = [
+            async next => {
+                mockFn('1 start')
+                await next()
+                mockFn('1 end')
+            },
+            async next => {
+                mockFn('2 start')
+                await next()
+                mockFn('2 end')
+            },
+            async next => {
+                mockFn('3 start')
+                await next()
+                mockFn('3 end')
+            },
+        ]
+        await compose(middlewares)()
+        
+        const calls = mockFn.mock.calls
+        expect(calls.length).toBe(6);
+        expect(calls[0][0]).toBe('1 start');
+        expect(calls[1][0]).toBe('2 start');
+        expect(calls[2][0]).toBe('3 start');
+        expect(calls[3][0]).toBe('3 end');
+        expect(calls[4][0]).toBe('2 end');
+        expect(calls[5][0]).toBe('1 end');
+    })
 })
