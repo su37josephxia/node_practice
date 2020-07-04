5e909ffd2c99cbb38ec51bc1d8e39c8ad533783c
xiaran <xiaran@polomi.com>
2020-07-04 09:49:49 +0800

    update add 异步函数测试用例

--- ex02/__tests__/index.spec.js
+++ ex02/__tests__/index.spec.js
@@ -1,25 +1,76 @@
-test('练习02 中间件实现', () => {
-    const mockFn = jest.fn()
 
-    const middlewares = [
-        async next => {
-            mockFn('1 start')
-            next()
-            mockFn('1 end')
-        },
-        async next => {
-            mockFn('2 start')
-            next()
-            mockFn('2 end')
-        }
-    ]
-    const { compose } = require('../index')
-    compose(middlewares)()
-    
-    const calls = mockFn.mock.calls
-    expect(calls.length).toBe(4);
-    expect(calls[0][0]).toBe('1 start');
-    expect(calls[1][0]).toBe('2 start');
-    expect(calls[2][0]).toBe('2 end');
-    expect(calls[3][0]).toBe('1 end');
+        const { compose } = require('../index')
+describe('练习02 中间件实现', () => {
+    it('同步函数' ,() => {
+        const mockFn = jest.fn()
+        const middlewares = [
+            async next => {
+                mockFn('1 start')
+                next()
+                mockFn('1 end')
+            },
+            async next => {
+                mockFn('2 start')
+                next()
+                mockFn('2 end')
+            }
+        ]
+        compose(middlewares)()
+        
+        const calls = mockFn.mock.calls
+        expect(calls.length).toBe(4);
+        expect(calls[0][0]).toBe('1 start');
+        expect(calls[1][0]).toBe('2 start');
+        expect(calls[2][0]).toBe('2 end');
+        expect(calls[3][0]).toBe('1 end');
+    })
+
+
+    it('异步函数' ,async () => {
+        const mockFn = jest.fn()
+        const middlewares = [
+            async next => {
+                mockFn('1 start')
+                await next()
+                mockFn('1 end')
+            },
+            async next => {
+                mockFn('2 start')
+                await next()
+                mockFn('2 end')
+            }
+        ]
+        await compose(middlewares)()
+        
+        const calls = mockFn.mock.calls
+        expect(calls.length).toBe(4);
+        expect(calls[0][0]).toBe('1 start');
+        expect(calls[1][0]).toBe('2 start');
+        expect(calls[2][0]).toBe('2 end');
+        expect(calls[3][0]).toBe('1 end');
+    })
+
+
+    it('混合同步异步的情况' ,async () => {
+        const mockFn = jest.fn()
+        const middlewares = [
+            async next => {
+                mockFn('1 start')
+                next()
+                mockFn('1 end')
+            },
+            next => {
+                mockFn('2 start')
+                mockFn('2 end')
+            }
+        ]
+        await compose(middlewares)()
+        
+        const calls = mockFn.mock.calls
+        expect(calls.length).toBe(4);
+        expect(calls[0][0]).toBe('1 start');
+        expect(calls[1][0]).toBe('2 start');
+        expect(calls[2][0]).toBe('2 end');
+        expect(calls[3][0]).toBe('1 end');
+    })
 })
