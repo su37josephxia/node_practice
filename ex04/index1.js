b25c8b1d4250dda3966c73ec12f3ddb6032825e2
zxlzl <384542246@qq.com>
2020-08-04 13:56:01 +0800

    answer

---
+++ ex04/index1.js
@@ -0,0 +1,34 @@
+const Sequelize = require("sequelize");
+module.exports.initModel = async (sequelize) => {
+  // ##BEGIN##
+  User = sequelize.define("user", {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: Sequelize.STRING,
+  });
+
+  Product = sequelize.define("product", {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+  });
+  Product.belongsTo(User, {
+    constraints: true,
+    onDelete: "CASCADE",
+  });
+  User.hasMany(Product);
+  await sequelize.sync(true);
+  // ##END##
+  return { User, Product };
+};
